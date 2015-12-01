03a6be3aabc0459f288f9a66d87e20fd9eb580e2
zhiming.tang <fortdam@gmail.com>
2015-12-01 22:55:36 +0800

    Update read_qualcomm_raw.m

--- toolbox/read_qualcomm_raw.m
+++ toolbox/read_qualcomm_raw.m
@@ -4,7 +4,7 @@
 % *file: The name of the file (string)
 % *w: The width of the picture
 % *h: The height of the picture
-% *format: Only 'Q10' is supported yet
+% *format: Support 'm10' or 'q10'
 
 function ret=read_qualcomm_raw(file, w, h, format)
     fp = fopen(file,'rb','b');
